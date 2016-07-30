0c50fc5278acf6523aa021224a9f6449e5c5421f
Francis Sullivan <francis@spiceworks.com>
2016-07-30 15:13:39 -0500

    add missing end

--- words.rb
+++ words.rb
@@ -1,7 +1,8 @@
 if $words.nil?
   $words = {}
   File.open("/usr/share/dict/words") do |file|
-  file.each do |line|
-    $words[line.strip] = true
+    file.each do |line|
+      $words[line.strip] = true
+    end
   end
 end
